096874fea11e0b0491130933c0a3f69049eecaf6
Nitsua365 <austinblanchard@hotmail.com>
2021-10-20 12:51:26 -0500

    Login WIP, and app can now be run in pyCharm env

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,19 +1,23 @@
 from app_package import app
-from flask import Flask, render_template, redirect, url_for, request
+from flask import Flask, render_template, redirect, url_for, request, flash
+from forms import LoginForm
 
-@app.route('/index', methods=['GET', 'POST'])
+# @app.route('/home')
+# def homePage():
+#     return render_template('homepage.html')
+#
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['Username'] != 'admin' or request.form['Password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            return redirect(url_for('/HomePage'))
-    return error
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/home')
+    return render_template('loginpage.html', title='Sign In', form=form)
 
 @app.route('/')
 @app.route('/index')
 def index():
-    loginStatus = login()
-    return render_template("loginpage.html", error=loginStatus)
+    form = LoginForm()
+    return render_template("loginpage.html")
 
