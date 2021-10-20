678f306a933cd724416197f2af0aedf992d55b15
Nitsua365 <austinblanchard@hotmail.com>
2021-10-19 21:29:22 -0500

    Login WIP

--- app_package/routes.py
+++ app_package/routes.py
@@ -1,8 +1,19 @@
 from app_package import app
-from flask import render_template
+from flask import Flask, render_template, redirect, url_for, request
+
+@app.route('/index', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['Username'] != 'admin' or request.form['Password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for('/HomePage'))
+    return error
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Richard'}
-    return render_template("homepage.html")
+    loginStatus = login()
+    return render_template("loginpage.html", error=loginStatus)
+
