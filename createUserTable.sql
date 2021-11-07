0fd27fc78207672cef826ec9cd8c3788c9fd1f35
Nitsua365 <austinblanchard@hotmail.com>
2021-11-07 16:47:58 -0600

    added a create user db script

---
+++ createUserTable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE User (
+    userId INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(100),
+    password VARCHAR(100),
+    firstName VARCHAR(255),
+    lastName VARCHAR(255)
+);
