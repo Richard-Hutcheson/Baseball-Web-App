1a4efac11655ce3f1154bf0aedb0082e56aaf671
Nitsua365 <austinblanchard@hotmail.com>
2021-11-08 10:17:16 -0600

    created person and User classes

--- app_package/Classes/User.py
+++ app_package/Classes/User.py
@@ -1,11 +1,11 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.ext.declarative import declarative_base
 
-Base = declartive_base()
+Base = declarative_base()
 
 class User(Base):
     __tablename__ = 'user'
 
-    userid=db.Column(Integer, primary_key=True)
-    firstName=db.Column(String)
-    lastName=db.Column(String)
+    userid=Column(Integer, primary_key=True)
+    firstName=Column(String)
+    lastName=Column(String)
