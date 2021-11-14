646ebc5bfae94ec1a64fe3cc6a50c7aa3873b15a
Nitsua365 <austinblanchard@hotmail.com>
2021-11-14 16:31:26 -0600

    cleaned pitching table and genTableQueries WIP

--- csv_cleaned/csvParser.py
+++ csv_cleaned/csvParser.py
@@ -37,6 +37,12 @@ def clean_managerCSV():
     df_half['isSeasonHalf'] = True
     df['isSeasonHalf'] = False
 
+    # make sure half data and regular data have the same columns
+    assert(len(df.columns.values) == len(df_half.columns.values))
+
+    for i in range(0, len(df.columns.values)):
+        assert(df.columns.values[i] == df_half.columns.values[i])
+
     df_combined = pd.concat([df, df_half])
 
     df_combined = df_combined.rename(columns={'playerID' : 'personID'})
@@ -67,8 +73,11 @@ def clean_battingCSV():
     df_batting.insert(loc=22, column='isPostSeason', value=[False for i in range(0, df_batting.shape[0])])
     df_batting_post.insert(loc=22, column='isPostSeason', value=[True for i in range(0, df_batting_post.shape[0])])
 
-    # check to make sure the
+    # check to make sure the number of columns is equal
     assert(len(df_batting.columns.values) == len(df_batting_post.columns.values))
+
+    for i in range(0, len(df_batting.columns.values)):
+        assert(df_batting.columns.values[i] == df_batting_post.columns.values[i])
 
     df_combined = pd.concat([df_batting, df_batting_post])
 
@@ -78,8 +87,36 @@ def clean_battingCSV():
 
 def clean_pitchingCSV():
     pit_file = 'Pitching.csv'
+    pit_file_post = 'PitchingPost.csv'
 
-    df_pit_file = pd.read_csv(pit_file, delimiter=',')
+    # create data frames for pitching and pitching post
+    df_pit = pd.read_csv(pit_file, delimiter=',')
+    df_pit_post = pd.read_csv(pit_file_post, delimiter=',')
+
+    # move the round column to the end of the row
+    df_pit_post_round_col = df_pit_post.pop('round')
+    df_pit_post.insert(loc=df_pit_post.shape[1], column='round', value=df_pit_post_round_col)
+
+    # add empty stint column to the post data
+    df_pit_post.insert(loc=2, column='stint', value=[None for i in range(0, df_pit_post.shape[0])])
+
+    # add empty round column to the end of pitching data frame
+    df_pit.insert(loc=df_pit.shape[1], column='round', value=[None for i in range(0, df_pit.shape[0])])
+
+    df_pit.insert(loc=df_pit.shape[1], column='isPostSeason', value=[False for i in range(0, df_pit.shape[0])])
+    df_pit_post.insert(loc=df_pit_post.shape[1], column='isPostSeason', value=[True for i in range(0, df_pit_post.shape[0])])
+
+    # check that the pitching data and the post pitching data has the same columns
+    assert(len(df_pit.columns.values) == len(df_pit_post.columns))
+
+    for i in range(0, len(df_pit.columns.values)):
+        assert(df_pit.columns.values[i] == df_pit_post.columns.values[i])
+
+    df_combined = pd.concat([df_pit, df_pit_post])
+
+    df_combined.sort_values(by=['yearID'], ascending=True, inplace=True)
+
+    saveCSV(df_combined, pit_file)
 
 
 def clean_playerCSV():
@@ -112,9 +149,9 @@ def main():
     os.chdir("../csv_files")
     clean_battingCSV()
 
-    # print("cleaning Players...")
-    # os.chdir("../csv_files")
-    # clean_playerCSV()
+    print("cleaning Pitching.csv...")
+    os.chdir("../csv_files")
+    clean_pitchingCSV()
 
 if __name__ == "__main__":
     main()
--- csv_cleaned/genTableQueries.sh
+++ csv_cleaned/genTableQueries.sh
@@ -4,10 +4,10 @@ files=`ls *.csv`
 
 echo $files
 
-
-
 for i in $files;
 do
-  echo "CREATE TABLE $i"
+  echo "CREATE TABLE $i ("
   head -1 $i | sed 's/,/ VARCHAR(100), /g'
 done
+
+echo " VARCHAR(100))"
