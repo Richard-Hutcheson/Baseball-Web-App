3f58350fcb670c170ac2157044854297512eac7c
Nitsua365 <austinblanchard@hotmail.com>
2021-11-29 17:37:05 -0600

    added more foreign keys and fixin debugging

--- csv_cleaned/baseballapp_Mac.sql
+++ csv_cleaned/baseballapp_Mac.sql
@@ -1,6 +1,6 @@
 USE baseballapp;
 
-
+SET FOREIGN_KEY_CHECKS = 0;
 
 DROP TABLE IF EXISTS AllStar;
 DROP TABLE IF EXISTS Awards;
@@ -10,22 +10,23 @@ DROP TABLE IF EXISTS Divisions;
 DROP TABLE IF EXISTS Fielding;
 DROP TABLE IF EXISTS FieldingOF;
 DROP TABLE IF EXISTS FieldingOFsplit;
-DROP TABLE IF EXISTS Franchises;
 DROP TABLE IF EXISTS HallOfFame;
 DROP TABLE IF EXISTS HomeGames;
-DROP TABLE IF EXISTS Leagues;
 DROP TABLE IF EXISTS Managers;
 DROP TABLE IF EXISTS Parks;
 DROP TABLE IF EXISTS Players;
 DROP TABLE IF EXISTS Pitching;
 DROP TABLE IF EXISTS People;
-DROP TABLE IF EXISTS PlayerPositions;
 DROP TABLE IF EXISTS PostSeasonSeries;
 DROP TABLE IF EXISTS Salaries;
 DROP TABLE IF EXISTS Schools;
 DROP TABLE IF EXISTS Teams;
 DROP TABLE IF EXISTS TeamsHalf;
+DROP TABLE IF EXISTS Leagues;
+DROP TABLE IF EXISTS Franchises;
+DROP TABLE IF EXISTS PlayerPositions;
 
+SET FOREIGN_KEY_CHECKS = 1;
 
 CREATE TABLE AllStar (
 AllStarRowID int NOT NULL AUTO_INCREMENT,
@@ -84,7 +85,7 @@ round VARCHAR(10)
 
 CREATE TABLE CollegePlaying (
 CollegePlayingRowID int NOT NULL AUTO_INCREMENT,
-personID VARCHAR(9) NOT NULL,
+personID VARCHAR(10) NOT NULL,
 schoolID VARCHAR(50),
 year smallint(6) NOT NULL,
 PRIMARY KEY (CollegePlayingRowID)
@@ -304,7 +305,7 @@ PRIMARY KEY (playerPosRowID)
 );
 
 CREATE TABLE Players (
-personID VARCHAR(100) NOT NULL,
+personID VARCHAR(10) NOT NULL,
 year smallint(6) NOT NULL,
 stint VARCHAR(100),
 teamID VARCHAR(3),
@@ -312,7 +313,7 @@ isPostSeason VARCHAR(2),
 isPitching VARCHAR(1),
 isBatting VARCHAR(1),
 isFielding VARCHAR(1),
-FOREIGN KEY (personID) REFERENCES People(PersonID)
+FOREIGN KEY(personID) REFERENCES People(personID)
 );
 
 CREATE TABLE PostSeasonSeries (
@@ -340,7 +341,7 @@ PRIMARY KEY (salaryRowID)
 );
 
 CREATE TABLE Schools (
-schoolID VARCHAR(50),
+schoolID VARCHAR(50) NOT NULL,
 schoolName CHAR(255),
 city CHAR(255),
 state CHAR(255),
@@ -1014,7 +1015,7 @@ ADD FOREIGN KEY (personID) REFERENCES People(personID);
 
 -- add FieldingOFsplit Foreign Keys
 ALTER TABLE FieldingOFsplit
-ADD FOREIGN KEY (personID) REFERENCES Person(personID);
+ADD FOREIGN KEY (personID) REFERENCES People(personID);
 
 ALTER TABLE FieldingOFsplit
 ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
@@ -1022,3 +1023,49 @@ ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
 -- add HallOfFame Foreign Keys
 ALTER TABLE HallOfFame
 ADD FOREIGN KEY (personID) REFERENCES People(personID);
+
+-- add HomeGames Foreign Keys
+ALTER TABLE HomeGames
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+ALTER TABLE HomeGames
+ADD FOREIGN KEY (parkID) REFERENCES Parks(parkID);
+
+-- add Managers Foreign Keys
+ALTER TABLE Managers
+ADD FOREIGN KEY (personID) REFERENCES People(personID);
+
+ALTER TABLE Managers
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+-- add Pitching Foreign Keys
+ALTER TABLE Pitching
+ADD FOREIGN KEY (personID) REFERENCES People(personID);
+
+ALTER TABLE Pitching
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+-- add PlayerPositions Foreign Keys
+ALTER TABLE PlayerPositions
+ADD FOREIGN KEY (personID) REFERENCES People(personID);
+
+ALTER TABLE PlayerPositions
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+-- add Salaries Foreign Keys
+ALTER TABLE Salaries
+ADD FOREIGN KEY (personID) REFERENCES People(personID);
+
+ALTER TABLE Salaries
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+-- add Teams Foreign Keys
+ALTER TABLE Teams
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
+
+ALTER TABLE Teams
+ADD FOREIGN KEY (franchID) REFERENCES Franchises(franchID);
+
+-- add TeamsHalf Foreign Keys
+ALTER TABLE TeamsHalf
+ADD FOREIGN KEY (leagueID) REFERENCES Leagues(leagueID);
