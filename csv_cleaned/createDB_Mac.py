56c754a820722c1c39f1780ebc0c14d4864062aa
Nitsua365 <austinblanchard@hotmail.com>
2021-11-30 20:04:19 -0600

    finished createDB script

--- csv_cleaned/createDB_Mac.py
+++ csv_cleaned/createDB_Mac.py
@@ -2,6 +2,7 @@ import pymysql as sql
 from csi3335fall2021 import user
 import os
 import re
+import subprocess
 
 def printCursor(cur):
     count = 0
@@ -13,9 +14,14 @@ def printCursor(cur):
     print("Result Set Size: " + str(count))
 
 def main():
+
     con = sql.connect(user=user['username'], password=user['password'], host=user['host'], db=user['db'])
 
     with con:
+
+        print("\ncreating CSVs...\n")
+        subprocess.call("python csvParser.py", shell=True)
+
         cur = con.cursor()
         SQL_Script_fname = 'baseballapp_Mac.sql'
 
@@ -38,6 +44,7 @@ def main():
 
         commands = list(map(lambda a: a + ';', commands))
 
+        print("\nbuilding database...\n")
         for i in range(0, len(commands)):
             cur.execute(commands[i])
 
